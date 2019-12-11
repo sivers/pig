3adb9b8081d3789957ae2fd8597feb964aca45ce
Derek Sivers <derek@sivers.org>
2019-12-11 08:58:17 +0000

    Ruby server good

--- README.md
+++ README.md
@@ -2,11 +2,17 @@ REST API webservers, getting JSON and status from PostgreSQL
 
 # INSTALL:
 
-
 ```
 # as your PostgreSQL super-user:
 createuser -s pig
 createdb -U pig pig
 psql -U pig -d pig -f pig.sql
+```
+
+## Ruby:
+
+```
+gem install pg sinatra thin
+ruby Ruby/pig.rb
 ```
 
--- Ruby/pig.rb
+++ Ruby/pig.rb
@@ -1,12 +1,24 @@
 require 'sinatra'
 require 'pg'
 require 'json'
+
+# database connection, to be used in request handling
 DB = PG::Connection.new(dbname: 'pig', user: 'pig')
-SCHEMA = File.read('../pig.sql')
+
+# read the database schema, to reset test data
+DIR = File.expand_path('../..', __FILE__)
+SQL = File.read(DIR + '/pig.sql')
+
+error Sinatra::NotFound do
+  content_type 'application/json'
+	[404, '{}']
+end
 
 class Pig
-  attr_reader :res
+  attr_accessor :res
 
+  # prefix every function call with this schema name
+  # (database has other schemas. we just one this one)
   def initialize(schema)
     @schema = schema
   end
@@ -16,32 +28,54 @@ class Pig
     '(%s)' % (1..params.size).map {|i| "$#{i}"}.join(',')
   end
 
-  # query database & use @res['status'] and @res['js'] below
+  # INPUT: function name, varargs parameters
+  # OUTPUT: @res['status'] (HTTP status), @res['js'] (JSON)
   def q(func, *params)
     @res = DB.exec_params("SELECT status, js FROM %s.%s%s" %
       [@schema, func, paramstring(params)], params)[0]
   end
 end
 
+# before processing every HTTP request...
 before do
-  # reset the database before every request
-  DB.exec(SCHEMA)
+  # get the apikey from HTTP header. quick fail if none or malformed
+  apikey = request.env['HTTP_APIKEY']
+  if apikey.nil? || !(/\A[a-z]{4}\Z/ === apikey)
+    halt 401, '{"error":"needs apikey header"}'
+  end
+
+  # reset the database test info
+  DB.exec(SQL)
+
+  # init database connection for use in all handlers
+  # 'pig' means every function will be prefixed with that schema name
   @pig = Pig.new('pig')
-  # TODO: get it from HTTP Request
-  apikey = 'aaaa'
+
+  # lookup person_id using apikey. remember it if found. fail now if not.
   @pig.q('apikey_get', apikey)
   if '200' == @pig.res['status']
     @person_id = JSON.parse(@pig.res['js'])['person_id']
+    @pig.res = nil
   else
-    halt 401, 'bad API key'
+    @pig = nil
+    halt 401, '{"error":"wrong apikey"}'
   end
 end
 
+# nice shortcut instead of manually passing status and js to response
+# with each request, the "after" filter does it every time
+# the "if" is only because of the halts in the "before" filter
 after do
   content_type 'application/json'
-  status @pig.res['status']
-  body @pig.res['js']
+	if @pig && @pig.res
+    status @pig.res['status']
+    body @pig.res['js']
+  end
 end
+
+# URI route handlers should be self-explanatory
+# Note that @person_id from auth stage is used, to limit user to
+# only get/set/update/delete their own things
 
 get '/' do
   @pig.q('people_get')
@@ -52,6 +86,7 @@ get %r{/person/([1-9][0-9]{0,5})} do |id|
 end
 
 patch '/person' do
+  halt(412, '{"error":"missing name"}') unless params[:name]
   @pig.q('person_update', @person_id, params[:name])
 end
 
@@ -64,10 +99,12 @@ get %r{/thing/([1-9][0-9]{0,5})} do |id|
 end
 
 patch %r{/thing/([1-9][0-9]{0,5})} do |id|
+  halt(412, '{"error":"missing name"}') unless params[:name]
   @pig.q('thing_update', @person_id, id, params[:name])
 end
 
 post '/things' do
+  halt(412, '{"error":"missing name"}') unless params[:name]
   @pig.q('thing_add', @person_id, params[:name])
 end
 
