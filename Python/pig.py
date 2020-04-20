8be4a53be5ffc2501e802c3570b168eada9884af
Rowland Carlson <rowland@stillclever.com>
2020-04-20 10:38:11 -0500

    added DIR and SQL

--- Python/pig.py
+++ Python/pig.py
@@ -1,3 +1,12 @@
 import psycopg2
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
 
 DB = psycopg2.connect("dbname=pig user=pig")
+
+DIR = os.path.abspath(".")
+with open(DIR + '/pig.sql') as f:
+    SQL = f.read()
