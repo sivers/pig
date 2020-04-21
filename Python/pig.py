617ad8e9bb46e9a425c77059b64d0e4bd60359ed
Rowland Carlson <rowland@stillclever.com>
2020-04-20 20:15:19 -0500

    added regex support for apikey and read apikey from header

--- Python/pig.py
+++ Python/pig.py
@@ -1,7 +1,9 @@
 import psycopg2
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 import os
 import psycopg2.extras
+import re
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -16,6 +18,10 @@ with open(DIR + '/pig.sql') as f:
 @app.errorhandler(404)
 def not_found(error):
     return jsonify(404, "{}")
+
+@app.errorhandler(401)
+def missing_apikey(error):
+    return jsonify(401, "{'error': 'needs apikey header'}")
 
 class Pig:
     def __init__(self, schema):
@@ -32,6 +38,38 @@ class Pig:
     def q(self, func, *params):
         DB.execute(f"SELECT status, js FROM {self.schema}.{func}{self.paramstring(len(params))}", params) 
         self.res = DB.fetchone()
+
+def before():
+    print('test wrapper')
+    apikey = request.headers.get('Apikey')
+    print(apikey)
+    print(apikey is None)
+    print(re.search("\A[a-z]{4}\Z", apikey))
+    print((apikey is None) or (re.search("\A[a-z]{4}\Z", apikey) is None))
+    if (apikey is None) or (re.search("\A[a-z]{4}\Z", apikey) is None):
+        abort(401)
+
+    pig_ = Pig('pig')
+
+def after():
+    print('after')
+    pass
+
+def before_and_after():
+    def decorator_func(func):
+        @wraps(func)
+        def wrapper_func(*args, **kwargs):
+            before()
+            retval = func(*args, **kwargs)
+            after()
+            return jsonify('test')
+        return wrapper_func
+    return decorator_func
+
+@app.route('/')
+@before_and_after()
+def people_get():
+    pig_.q('people_get')
 
 if __name__ == "__main__":
     app.run(debug=True)
